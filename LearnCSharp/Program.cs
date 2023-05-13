8fd2d1b863580af64df51acb7afcf0e9c7d4379b
Taizy <48052195+snrelghgub@users.noreply.github.com>
2023-05-13 22:02:01 +1000

    Add project files.

---
+++ LearnCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿while (true)
+{
+    DateTime currentTime = DateTime.Now;
+    string date = currentTime.ToString("dd/MM/yyyy");
+    string time = currentTime.ToString("hh:mm:ss tt");
+
+    Console.WriteLine(date + " - " + time);
+
+    Thread.Sleep(1000);
+    Console.Clear();
+}
+
