5ac45af3080706db7e2982836a49d8b76d040929
Taizy <48052195+snrelghgub@users.noreply.github.com>
2023-06-18 00:57:13 +1000

    Update Program.cs

--- LearnCSharp/Program.cs
+++ LearnCSharp/Program.cs
@@ -5,6 +5,22 @@ Console.WriteLine("Learn C#");
 Console.ResetColor();
 
 Console.WriteLine();
+Console.WriteLine("1. Draw a shape");
+Console.WriteLine();
+
+int width = 10;  // width of the rectangle
+int height = 5;  // height of the rectangle
+
+for (int i = 0; i < height; i++)
+{
+    for (int j = 0; j < width; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
 Console.WriteLine("2. Use variables");
 Console.WriteLine();
 
@@ -40,8 +56,8 @@ string codeExplanation2 = @"
 This code uses the built-in 'DateTime' data type in C# to extract 
 the current date and time. The variable 'currentTime' is a new 
 instance of the 'DateTime' data type, while 'date' and 'time' are 
-strings that will hold the formatted date and time values, respectively. 
-The 'ToString()' method is used to extract the month data from 
+string variables that will hold the formatted date and time values, 
+respectively. The 'ToString()' method is used to extract the month data from 
 'currentTime' using ""MM"" and the minutes data using ""mm"" etc. 
 The Sleep method sets a pause to the while loop for one second, 
 allowing the clock to update correctly. The 'Console.Clear()' method 
@@ -49,7 +65,8 @@ is used to clear the console screen to update the clock every second.
 The final result is a clock that displays both the 'date' and 'time' accurately. 
 The 'Sleep()' method is part of the System.Threading namespace in C#. The 'WriteLine' 
 method comes from the Console class in C#, which is part of the .Net Framework 
-class library in the System namespace. Other examples of classes 
-in the System namespace are 'DateTime' and 'ConsoleColor'.";
+class library in the System namespace used to print the date & time string 
+variables to the console. Other examples of classes in the System namespace 
+are 'DateTime' and 'ConsoleColor'.";
 
 Console.WriteLine(codeExplanation2);
