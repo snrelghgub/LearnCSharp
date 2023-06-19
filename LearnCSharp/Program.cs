124b0f583dd0a5a716db8e27228a10450331de67
Taizy <48052195+snrelghgub@users.noreply.github.com>
2023-06-19 22:43:05 +1000

    add each person to string arr & data occurrences

--- LearnCSharp/Program.cs
+++ LearnCSharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Faker;
+using System.Text.RegularExpressions;
 
 Console.ForegroundColor = ConsoleColor.Green;
 Console.BackgroundColor = ConsoleColor.DarkBlue;
@@ -118,12 +119,12 @@ Console.WriteLine();
 // Person 1
 string name1 = Faker.Name.FullName(NameFormats.Standard);
 string address1 = Faker.Address.StreetAddress() + ", " + Faker.Address.City() + ", " + Faker.Address.UsState() + ", " + Faker.Address.ZipCode();
-string person1 = String.Concat("Name: ", name1,"\nAddress: ", address1, "\nLandline number: ");
+string person1 = String.Concat("Name: ", name1,"\nAddress: ", address1);
 
 // Person 2
 string name2 = Faker.Name.FullName(NameFormats.Standard);
 string landlineNumber2 = Faker.Phone.Number().Replace(".", "-").Split('x')[0].Trim();
-string person2 = String.Concat("Name: ", name2, "\nAddress: ", "\nLandline number: ", landlineNumber2);
+string person2 = String.Concat("Name: ", name2, "\nLandline number: ", landlineNumber2);
 
 // Person 3
 string name3 = Faker.Name.FullName(NameFormats.Standard);
@@ -131,13 +132,16 @@ string address3 = Faker.Address.StreetAddress() + ", " + Faker.Address.City() + 
 string landlineNumber3 = Faker.Phone.Number().Replace(".", "-").Split('x')[0].Trim();
 string person3 = String.Concat("Name: ", name3, "\nAddress: ", address3, "\nLandline number: ", landlineNumber3);
 
-// Person 1
-Console.WriteLine(person1);
-Console.WriteLine();
+string[] people = new string[3];
+people[0] = person1;
+people[1] = person2;
+people[2] = person3;
 
-//Person 2
-Console.WriteLine(person2);
-Console.WriteLine();
-
-//Person 3
-Console.WriteLine(person3);
+foreach (string person in people)
+{
+    Console.WriteLine(person);
+    Console.WriteLine($"{(person.Contains("Name") ? 1 : 0)} name was found.");
+    Console.WriteLine($"{(person.Contains("Address") ? 1 : 0)} address was found.");
+    Console.WriteLine($"{(person.Contains("Landline number") ? 1 : 0)} landline number was found.");
+    Console.WriteLine();
+}
