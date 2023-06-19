111384d1252c6016ae5aeb54bbc78c023dfe7a11
Taizy <48052195+snrelghgub@users.noreply.github.com>
2023-06-19 22:55:33 +1000

    add new code example

--- LearnCSharp/Program.cs
+++ LearnCSharp/Program.cs
@@ -116,20 +116,17 @@ Console.WriteLine();
 Console.WriteLine("4. Work with strings");
 Console.WriteLine();
 
-// Person 1
 string name1 = Faker.Name.FullName(NameFormats.Standard);
 string address1 = Faker.Address.StreetAddress() + ", " + Faker.Address.City() + ", " + Faker.Address.UsState() + ", " + Faker.Address.ZipCode();
-string person1 = String.Concat("Name: ", name1,"\nAddress: ", address1);
+string person1 = String.Concat("Name: ", name1, "\nAddress: ", address1);
 
-// Person 2
 string name2 = Faker.Name.FullName(NameFormats.Standard);
-string landlineNumber2 = Faker.Phone.Number().Replace(".", "-").Split('x')[0].Trim();
+string landlineNumber2 = Faker.Phone.Number().Replace(".", "-").Split("x")[0].Trim();
 string person2 = String.Concat("Name: ", name2, "\nLandline number: ", landlineNumber2);
 
-// Person 3
 string name3 = Faker.Name.FullName(NameFormats.Standard);
 string address3 = Faker.Address.StreetAddress() + ", " + Faker.Address.City() + ", " + Faker.Address.UsState() + ", " + Faker.Address.ZipCode();
-string landlineNumber3 = Faker.Phone.Number().Replace(".", "-").Split('x')[0].Trim();
+string landlineNumber3 = Faker.Phone.Number().Replace(".", "-").Split("x")[0].Trim();
 string person3 = String.Concat("Name: ", name3, "\nAddress: ", address3, "\nLandline number: ", landlineNumber3);
 
 string[] people = new string[3];
@@ -145,3 +142,33 @@ foreach (string person in people)
     Console.WriteLine($"{(person.Contains("Landline number") ? 1 : 0)} landline number was found.");
     Console.WriteLine();
 }
+
+string codeExample4 = @"
+string name1 = Faker.Name.FullName(NameFormats.Standard);
+string address1 = Faker.Address.StreetAddress() + "", "" + Faker.Address.City() + "", "" + Faker.Address.UsState() + "", "" + Faker.Address.ZipCode();
+string person1 = String.Concat(""Name: "", name1, ""\nAddress: "", address1);
+
+string name2 = Faker.Name.FullName(NameFormats.Standard);
+string landlineNumber2 = Faker.Phone.Number().Replace(""."", ""-"").Split(""x"")[0].Trim();
+string person2 = String.Concat(""Name: "", name2, ""\nLandline number: "", landlineNumber2);
+
+string name3 = Faker.Name.FullName(NameFormats.Standard);
+string address3 = Faker.Address.StreetAddress() + "", "" + Faker.Address.City() + "", "" + Faker.Address.UsState() + "", "" + Faker.Address.ZipCode();
+string landlineNumber3 = Faker.Phone.Number().Replace(""."", ""-"").Split(""x"")[0].Trim();
+string person3 = String.Concat(""Name: "", name3, ""\nAddress: "", address3, ""\nLandline number: "", landlineNumber3);
+
+string[] people = new string[3];
+people[0] = person1;
+people[1] = person2;
+people[2] = person3;
+
+foreach (string person in people)
+{
+    Console.WriteLine(person);
+    Console.WriteLine($""{(person.Contains(""Name"") ? 1 : 0)} name was found."");
+    Console.WriteLine($""{(person.Contains(""Address"") ? 1 : 0)} address was found."");
+    Console.WriteLine($""{(person.Contains(""Landline number"") ? 1 : 0)} landline number was found."");
+    Console.WriteLine();
+}";
+
+Console.WriteLine(codeExample4);
