ed94e66a38cad977e0510f935cd6c3da4bc35949
Taizy <48052195+snrelghgub@users.noreply.github.com>
2023-07-29 20:37:04 +1000

    add props, constructor & method to new Student class

---
+++ LearnCSharp/Student.cs
@@ -0,0 +1,22 @@
+﻿namespace LearnCSharp
+{
+    internal class Student
+    {
+        private int StudentId { get; set; }
+        private string? Name { get; set; }
+        private DateTime DateOfBirth { get; set; }
+        private bool IsOnScholarship { get; set; }
+
+        public Student(int studentId, string name, DateTime dateOfBirth, bool isOnScholarship)
+        {
+            this.StudentId = studentId;
+            this.Name = name;
+            this.DateOfBirth = dateOfBirth;
+            this.IsOnScholarship = isOnScholarship;
+        }
+        public bool HasScholarship()
+        {
+            return this.IsOnScholarship;
+        }
+    }
+}
