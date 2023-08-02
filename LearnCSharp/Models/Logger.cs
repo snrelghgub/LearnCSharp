cdd7162dc9e3f48d672dd033456649b0a08b5078
Taizy <48052195+snrelghgub@users.noreply.github.com>
2023-08-02 16:36:30 +1000

    add new Logger static class & methods

---
+++ LearnCSharp/Models/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnCSharp.Models
+{
+    internal static class Logger
+    {
+        public static void LogListType<T>(List<T> list)
+        {
+            Console.WriteLine($"List type: {typeof(T).Name}");
+        }
+
+        public static void DebugLog(string message)
+        {
+            Console.WriteLine($"[DEBUG]: {message}");
+        }
+    }
+}
