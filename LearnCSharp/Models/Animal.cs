de7e5018d29b2bd997fab411c101b5c8010d691f
Taizy <48052195+snrelghgub@users.noreply.github.com>
2023-08-05 23:40:41 +1000

    add new classes (use inheritance & override class method)

---
+++ LearnCSharp/Models/Animal.cs
@@ -0,0 +1,37 @@
+﻿namespace LearnCSharp.Models
+{
+    internal class Animal
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public int Weight { get; set; }
+        public Animal(string? name, int age, int weight)
+        {
+            Name = name;
+            Age = age;
+            Weight = weight;
+        }
+        public virtual string AnimalSound()
+        {
+            return "makes (unknown) sounds!";
+        }
+    }
+
+    internal class Dog : Animal
+    {
+        public Dog(string? name, int age, int weight) : base(name, age, weight) { }
+        public override string AnimalSound()
+        {
+            return "barks!";
+        }
+    }
+
+    internal class Cat : Animal
+    {
+        public Cat(string? name, int age, int weight) : base(name, age, weight) { }
+        public override string AnimalSound()
+        {
+            return "meows!";
+        }
+    }
+}
