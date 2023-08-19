bba12f83e40df85df9187fce9e507abb5a3bd9fb
Taizy <48052195+snrelghgub@users.noreply.github.com>
2023-08-19 20:46:36 +1000

    remove unwanted space in code snippet

--- LearnCSharp/Program.cs
+++ LearnCSharp/Program.cs
@@ -34,7 +34,7 @@ for (int i = 0; i < height; i++)
 {
     for (int j = 0; j < width; j++)
     {
-        Console.Write("" * "");
+        Console.Write(""*"");
     }
     Console.WriteLine();
 }";
